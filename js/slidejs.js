f09e3d90ba448f9a27ea0ba8ff79cbd39813c0c0
chantelletjie <chantellelandman710@gmail.com>
2019-04-14 15:00:30 +0200

    ID connection not fixed yet

--- js/slidejs.js
+++ js/slidejs.js
@@ -1,19 +1,20 @@
 
+
+
 var slideIndex = 1;
 showDivs(slideIndex);
 
-function plusDivs(n) {
+function plusDivs(n) {                        //arrow functions
   showDivs(slideIndex += n);
 }
 
 function showDivs(n) {
-  var i;
-  var x = document.getElementsByClassName("mySlides");
-  if (n > x.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = x.length}
+  var i;                                                  // Array or index variable  
+  var x = document.getElementsByClassName("mySlides");//grabs the images from class name mySlides
+  if (n > x.length) {slideIndex = 1}                  //adds next slide 
+  if (n < 1) {slideIndex = x.length}                  //subtracts present slide
   for (i = 0; i < x.length; i++) {
-     x[i].style.display = "none";
+     x[i].style.display = "none";                     // When image isnt displayed
   }
-  x[slideIndex-1].style.display = "flex";
+  x[slideIndex-1].style.display = "flex";             //when image is on display
 }
-
